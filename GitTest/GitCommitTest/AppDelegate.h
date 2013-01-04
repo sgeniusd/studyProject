d718949f0ab6d3b5d6ad494d4f05c51ccdc1552d
Magic Song <magic@Magic-SongmatoiMac.local>
2013-01-04 16:06:22 +0800

    添加测试代码

---
+++ GitTest/GitCommitTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GitCommitTest
+//
+//  Created by Magic Song on 13-1-4.
+//  Copyright (c) 2013年 Magic Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GitTest/GitCommitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitCommitTest
+//
+//  Created by Magic Song on 13-1-4.
+//  Copyright (c) 2013年 Magic Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
